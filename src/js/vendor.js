40f7620e169fc0cecc6129e643026ccb69c5a194
Denis Kulikov <altingfest@gmail.com>
2016-03-25 07:50:17 +0300

    v1.0

---
+++ src/js/vendor.js
@@ -0,0 +1,13 @@
+/**************************************/
+/* Vendor JavaScript files supervisor */
+/**************************************/
+
+/* Third party */
+
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/jquery/jquery-ui.js
+//= ../../bower_components/tooltip/bootstrap.js
+
+//= ../../bower_components/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js
+
+
